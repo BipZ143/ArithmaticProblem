55fdb23a4ea1929ded6932406159ec9f862bb09b
BipZ143 <sahoobhanupratap2@gmail.com>
2020-08-04 19:06:42 +0530

    Descending Order

---
+++ descending.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -x
+echo enter the no of element
+read n1
+for ((i=0; i<$n1; i++))
+do
+	echo enter `expr $i + 1` the element.
+	read a[$i]
+done
+for (( i=0; i<$n1; i++ ))
+do
+	for (( j=`expr $i + 1`; j<$n1; j++ ))
+	do
+		if [ ${a[$i]} -gt ${a[$j]} ]
+		then
+			x=${a[$i]}
+			a[$i]=${a[$j]}
+			a[$j]=$x
+		fi
+	done
+done
+echo "The Descending order is: "
+for (( i=$n1; i>0; i-- ))
+do
+	echo ${a[`expr $i - 1`]}
+done
