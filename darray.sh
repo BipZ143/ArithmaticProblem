c965d5d7c8228539cd4fba79cbdb32a8d2a2eeaa
Bhanupratap Sahoo <31233787+BipZ143@users.noreply.github.com>
2020-08-12 23:32:09 +0530

    Update darray.sh

--- darray.sh
+++ darray.sh
@@ -1,4 +1,22 @@
 #!/bin/bash -x
-declare -A arr
-arr["key1"]=val1
-arr+=( ["key2"]=val2 ["key3"]=val3 )
+
+read -p "Enter input a : " a
+read -p "Enter input b : " b
+read -p "Enter input c : " c
+
+declare -A operationDic
+declare -a operationArray
+operation1=$(($a+$b*$c))
+operation2=$(($a*$b+$c))
+operation3=$(($a+$b/$c))
+operation4=$(($a%$b+$c))
+
+for (( i=1 ; i<=4 ; i++ ))
+do
+	operationDic[$i]=$((operation$i))
+done
+
+for (( j=0 ; j<4 ; j++ ))
+do
+	operationArray[$j]=${operationDic[$(($j+1))]}
+done
