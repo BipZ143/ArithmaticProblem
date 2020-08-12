281cc06c349e1aca23ad84d09806c169f927afa1
Bhanupratap Sahoo <31233787+BipZ143@users.noreply.github.com>
2020-08-12 23:30:57 +0530

    Update dict.sh

--- dict.sh
+++ dict.sh
@@ -1,14 +1,16 @@
 #!/bin/bash -x
-declare -A value
-read -p "Enter value for a: " a
-read -p "Enter value for b: " b
-read -p "Enter value for c: " c
-d=$((a+b*c))
-e=$((a*b+c))
-f=$((c+a/b))
-g=$((a%b+c))
-echo "Result is" $d
-echo "Result is" $e
-echo "Result is" $f
-echo "Result is" $g
 
+read -p "Enter input a : " a
+read -p "Enter input b : " b
+read -p "Enter input c : " c
+
+declare -A operationDic
+operation1=$(($a+$b*$c))
+operation2=$(($a*$b+$c))
+operation3=$(($a+$b/$c))
+operation4=$(($a%$b+$c))
+
+for (( i=1 ; i<=4 ; i++ ))
+do
+	operationDic[$i]=$((operation$i))
+done
